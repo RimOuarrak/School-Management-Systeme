2b8e568462926615f4edb42592d596b3cd7ee38d
meDski101 <79527266+meDski101@users.noreply.github.com>
2021-10-19 21:56:12 +0100

    add Main class

---
+++ School_Management_Systeme/src/mypackage/Main.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public final class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
